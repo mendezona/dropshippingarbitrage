edb115f6d8c2d73d25194d6dbbca34a28cbc613e
Joseph <joseph_mendezona@hotmail.com>
2024-05-01 21:59:44 +0200

    add discord authentication

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,10 +1,12 @@
-import { LoginForm } from '@/components/ui/loginForm/login-form';
+'use client';
+
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 export default function LoginPage() {
   return (
     <main className="flex items-center justify-center md:h-screen">
       <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-        <LoginForm />
+        <button onClick={() => signIn('discord')}>Sign in with Discord</button>
       </div>
     </main>
   );
